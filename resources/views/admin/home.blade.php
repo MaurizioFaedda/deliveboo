17582a4ef92f7088c471cb42848040743d098da8
Marco <markoka1@outlook.it>
2021-02-12 17:08:18 +0100

    Inserimento della zona crud nella dashboard

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -14,7 +14,62 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Benvenuto!') }}
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-center my-4">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header text-center">
+                    <button type="button" class="btn btn-dark btn-lg btn-block">Inserisci nuovi piatti.</button>
+                </div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    <ul class="list-group">
+                        <li class="list-group-item d-flex justify-content-between">
+                            <p>Item</p>
+                            <div>
+                                <button type="button" class="btn btn-secondary">Modifica</button>
+                                <button type="button" class="btn btn-secondary">Cancella</button>
+                            </div>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between">
+                            <p>Item</p>
+                            <div>
+                                <button type="button" class="btn btn-secondary">Modifica</button>
+                                <button type="button" class="btn btn-secondary">Cancella</button>
+                            </div>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between">
+                            <p>Item</p>
+                            <div>
+                                <button type="button" class="btn btn-secondary">Modifica</button>
+                                <button type="button" class="btn btn-secondary">Cancella</button>
+                            </div>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between">
+                            <p>Item</p>
+                            <div>
+                                <button type="button" class="btn btn-secondary">Modifica</button>
+                                <button type="button" class="btn btn-secondary">Cancella</button>
+                            </div>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between">
+                            <p>Item</p>
+                            <div>
+                                <button type="button" class="btn btn-secondary">Modifica</button>
+                                <button type="button" class="btn btn-secondary">Cancella</button>
+                            </div>
+                        </li>
+                    </ul>
+
                 </div>
             </div>
         </div>
