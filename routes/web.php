0f7639162d9732bef5a59852cbd6a33e2ad6dff4
Maurizio <maurizio.faedda1@gmail.com>
2021-02-12 15:59:55 +0100

    Dashboard Layouts

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index')->name('index');
+
 
 Auth::routes();
 
