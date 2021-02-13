f6812103ddc4a06c3c8c67b2cf39c3cdb461a8d8
Sabrina <sabrinacunsolo85@gmail.com>
2021-02-13 12:46:41 +0100

    create seeder Restaurants table

---
+++ config/restaurants.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    [
+        "restaurant_name" => "Pizzeria da Giovanni",
+        "city" => "Roma",
+        "address" => "Via Condotti 1",
+        "user_id" => 1
+    ],
+    [
+        "restaurant_name" => "Ristorante Bianchi",
+        "city" => "Roma",
+        "address" => "Corso Garibaldi 15",
+        "user_id" => 2
+    ],
+    [
+        "restaurant_name" => "Hanami",
+        "city" => "Roma",
+        "address" => "Via Milano 23",
+        "user_id" => 3
+    ],
+    [
+        "restaurant_name" => "Vegan Restaurant",
+        "city" => "Roma",
+        "address" => "Via Caffaro 5",
+        "user_id" => 4
+    ],
+    [
+        "restaurant_name" => "Bottega del Bio",
+        "city" => "Roma",
+        "address" => "Via Palermo 45",
+        "user_id" => 5
+    ],
+
+];
---
+++ database/seeds/RestaurantsTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+use App\Restaurant;
+
+class RestaurantsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $restaurants = config('restaurants');
+
+        foreach ($restaurants as $restaurant) {
+            $new_restaurant_obj = new Restaurant();
+            $new_restaurant_obj->restaurant_name = $restaurant['restaurant_name'];
+            $new_restaurant_obj->city = $restaurant['city'];
+            $new_restaurant_obj->address = $restaurant['address'];
+            $new_restaurant_obj->user_id = $restaurant['user_id'];
+            $new_restaurant_obj->save();
+        }
+    }
+
+}
