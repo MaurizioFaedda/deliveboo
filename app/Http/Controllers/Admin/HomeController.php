92a1c68acc439cef8c20c1fd43fe7cbe9ae7552f
Maurizio <maurizio.faedda1@gmail.com>
2021-02-15 17:26:41 +0100

    Create count restaurants if

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -5,11 +5,17 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Restaurant;
+use App\User;
 
 class HomeController extends Controller
 {
     public function index()
-    {
-        return view('admin.home');
+    {   $user_current = Auth::user()->id;
+        $data=[
+            'countRestaurants'=> Restaurant::where('user_id', $user_current)->count()
+        ];
+        // dd($data);
+        return view('admin.home', $data);
     }
 }
