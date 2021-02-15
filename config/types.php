87a70ad43c3fe387e2fa3d9d5b2641ac71011d01
PatriziaCrimi <crimi.patrizia@gmail.com>
2021-02-15 10:46:53 +0100

    Create Types seeder

---
+++ config/types.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+  [
+    "type_name" => "Pizzeria",
+    "notes" => "",
+  ],
+  [
+    "type_name" => "Cucina italiana",
+    "notes" => "",
+  ],
+  [
+    "type_name" => "Sushi",
+    "notes" => "",
+  ],
+  [
+    "type_name" => "Cucina vegana",
+    "notes" => "",
+  ],
+  [
+    "type_name" => "Cucina bio",
+    "notes" => "",
+  ],
+];
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsersTableSeeder::class);
         $this->call(RestaurantsTableSeeder::class);
+        $this->call(TypesTableSeeder::class);
     }
 }
