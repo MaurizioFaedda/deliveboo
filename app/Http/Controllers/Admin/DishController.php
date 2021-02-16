5a96171c1c8196f8989695bdbfd5b653cd780d2e
PatriziaCrimi <crimi.patrizia@gmail.com>
2021-02-16 16:21:21 +0100

    CRUD show DishesController (secure your dishes)

--- app/Http/Controllers/Admin/DishController.php
+++ app/Http/Controllers/Admin/DishController.php
@@ -29,13 +29,13 @@ class DishController extends Controller
    */
   public function create(Request $request)
   {
-      $query = $request->input('restaurant');
+    $query = $request->input('restaurant');
 
-      $data = [
-        'restaurants' => Restaurant::all(),
-        'query' => $query
-      ];
-      return view('admin.dishes.create', $data);
+    $data = [
+      'restaurants' => Restaurant::all(),
+      'query' => $query
+    ];
+    return view('admin.dishes.create', $data);
   }
 
   /**
@@ -46,23 +46,23 @@ class DishController extends Controller
    */
   public function store(Request $request)
   {
-      $request->validate([
-        'name' => 'required|max:30',
-        'infos' => 'required|max:250',
-        'visible' => 'required',
-        'price' => 'required|numeric|between:0,999.99',
-      ]);
+    $request->validate([
+      'name' => 'required|max:30',
+      'infos' => 'required|max:250',
+      'visible' => 'required',
+      'price' => 'required|numeric|between:0,999.99',
+    ]);
 
-      // Storing all form data in a variable
-      $form_data = $request->all();
-      // Creating a new Object/Instance with the form data
-      $new_dish = new Dish();
-      $new_dish->fill($form_data);
-      // Saving the new Object/Instance in the database
-      $new_dish->save();
-      // Redirecting to the view with all posts
-      $restaurant_id = $request->restaurant_id;
-      return redirect()->route('admin.restaurants.show', ['restaurant' => $restaurant_id]);
+    // Storing all form data in a variable
+    $form_data = $request->all();
+    // Creating a new Object/Instance with the form data
+    $new_dish = new Dish();
+    $new_dish->fill($form_data);
+    // Saving the new Object/Instance in the database
+    $new_dish->save();
+    // Redirecting to the view with all posts
+    $restaurant_id = $request->restaurant_id;
+    return redirect()->route('admin.restaurants.show', ['restaurant' => $restaurant_id]);
   }
 
   /**
@@ -73,70 +73,29 @@ class DishController extends Controller
    */
   public function show($id)
   {
-
-    // // Prendo l'id dell'utente autenticato
-    // $id_user = Auth::user()->id;
-    //
-    // // Query per prendere il piatto cliccato
-    // $dish = Dish::where('id', $id)->first();
-    //
-    // // Controllo che il Piatto visualizzato sia dell'utente autenticato e che il piatto esista
-    // if($id_user == $restaurant->user_id && $dish) {
-    //
-    //     $data = [
-    //         'dish' => $dish
-    //     ];
-    //
-    //     return view('admin.dishes.show', $data);
-    // } else {
-    //     abort('404');
-    // }
-
     // Prendo l'id dell'utente autenticato
-    $id_user = Auth::user()->id;
+    $current_user = Auth::user()->id;
 
-    // Prendo l'id dell'utente nella tabella restaurants che deve essere uguale all'id utente loggato
-    $myqueryrestaurant = DB::table('restaurants')->select('user_id')->where('user_id', '=', $id_user)->orderBy('id','desc')->get();
-    // Restituisce l'id dell'tente loggato in quel ristorante
-    $id_user_fk = $myqueryrestaurant[0]->user_id;
+    // SELECT * FROM dishes WHERE 'id' = $id (variabile passata)
+    $current_dish = Dish::find($id);
+    // Prendo il ristorante corrente dalla Collection $current_dish
+    $current_restaurant_fk = $current_dish->restaurant_id;
 
-    // Visualizzare il piatto in cui la foreign key $restaurant_id è uguale alla primary key(id) della tabella restaurants dove (query) la foreign key user_id è uguale all'utente loggato($id_user_fk).
+    // QUERY per prendere il ristorante corrente
+    $current_restaurant_collection = Restaurant::where('user_id', $current_user)->first();
+    // Prendo il ristorante corrente dalla Collection
+    $current_restaurant = $current_restaurant_collection->id;
+    // Prendo la FK dello user dalla tabella Restaurants (nella Collection del ristorante corrente)
+    $current_user_fk = $current_restaurant_collection->user_id;
 
-
-    $current_restaurant = DB::table('dishes')->select('restaurant_id')->join('restaurants', 'id', '=', 'restaurant_id')->get();
-    dd($current_restaurant);
-
-
-
-
-
-
-    // Prendo l'id di ristorante nella tabella ristoranti dove l'id user deve essere uguale all'utente autenticato
-    $myquery_id_restaurant = DB::table('restaurants')->select('id')->where( $id_user_fk, '=', $id_user)->orderBy('id','desc')->get();
-    $id_restaurant = $myquery_id_restaurant[0]->id;
-    dd($id_restaurant);
-
-    // Prendo l'id del ristorante nella tabella dishes
-    $myquerydish = DB::table('dishes')->select('restaurant_id')->where('restaurant_id', '=', $id_restaurant)->orderBy('id','desc')->get();
-    $id_restaurant_fk = $myquerydish[0]->restaurant_id;
-
-    // Query per prendere il piatto cliccato
-    $dish = Dish::where('id', $id)->first();
-
-    // Controllo che il Piatto visualizzato sia dell'ruistorante cliccato e che il piatto esista
-    if($id_restaurant === $id_restaurant_fk) {
-
-        $data = [
-            'dish' => $dish
-        ];
-
-        return view('admin.dishes.show', $data);
-
-    } else {
-        abort('404');
+    // Controllo che il piatto corrente esista && che appartenga al ristorante corrente && che il ristorante sia dell'utente loggato
+    if($current_dish && $current_restaurant_fk == $current_restaurant && $current_user == $current_user_fk) {
+      $data = [
+        'dish' => $current_dish,
+      ];
+      return view('admin.dishes.show', $data);
     }
-
-
+    abort(404);
   }
 
   /**
