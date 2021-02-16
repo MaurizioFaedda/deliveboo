afe45f05912547d2e7deb132136550fb9f6eb244
Marco <markoka1@outlook.it>
2021-02-16 12:49:52 +0100

    sistemato bordi card

--- resources/views/admin/restaurants/show.blade.php
+++ resources/views/admin/restaurants/show.blade.php
@@ -6,7 +6,7 @@
 
             {{--              FORM PER CREAZIONE PIATTO             --}}
             <div class="col-md-4 col-sm-12">
-              <div class="card no-border mt-3">
+              <div class="card no-border border-radius-top mt-3">
                 <div class="card-header card-header text-center no-border border-radius-top p-3">
                     <h5 class="mb-0">Aggiungi un nuovo piatto</h5>
                 </div>
@@ -74,7 +74,7 @@
             {{--              LISTA CON TUTTI I PIATTI             --}}
 
             <div class="col-md-8 col-sm-12">
-                <div class="card no-border mt-3">
+                <div class="card no-border border-radius-top mt-3">
                     <div class="card-header text-center no-border border-radius-top font-weight-bold">
                         Ristorante {{$restaurant->restaurant_name}}
                     </div>
