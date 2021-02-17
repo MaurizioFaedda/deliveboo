2642866949719104d5dad450b32d400b2e94e75b
Maurizio <maurizio.faedda1@gmail.com>
2021-02-17 20:06:00 +0100

    bug fix show restaurant responsive

--- resources/views/admin/restaurants/show.blade.php
+++ resources/views/admin/restaurants/show.blade.php
@@ -4,37 +4,40 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-12 d-flex mb-5">
-                <div class="col-md-4 col-sm-12">
-                    <div class="card no-border border-radius-top shadow p-4">
-                        @if($restaurant->img_path_rest)
-                            <div class="img-wrapper py-5 h-100">
-                                <img class="img-fluid" src="{{ asset('storage/' . $restaurant->img_path_rest)}}" alt="{{$restaurant->restaurant_name}}">
+                <div class="row">
+                    <div class="col-md-4 col-sm-12">
+                        <div class="card no-border border-radius-top shadow p-4">
+                            @if($restaurant->img_path_rest)
+                                <div class="img-wrapper py-5 h-100">
+                                    <img class="img-fluid" src="{{ asset('storage/' . $restaurant->img_path_rest)}}" alt="{{$restaurant->restaurant_name}}">
+                                </div>
+                            @else
+                                <img src="{{ asset('img/img_not_available.png')}}" alt="not img">
+                            @endif
+                            <div>
+                                <ul>
+                                    @forelse ($restaurant->types as $type)
+                                        <li>{{ $type->type }}</li>
+                                    @empty
+                                        <li>Non sono state inserite tipologie</li>
+                                    @endforelse
+                                </ul>
                             </div>
-                        @else
-                            <img src="{{ asset('img/img_not_available.png')}}" alt="not img">
-                        @endif
-                        <div>
-                            <ul>
-                                @forelse ($restaurant->types as $type)
-                                <li>{{ $type->type }}</li>
-                                @empty
-                                <li>Non sono state inserite tipologie</li>
-                                @endforelse
-                            </ul>
                         </div>
                     </div>
-                </div>
-                <div class="col-md-8 col-sm-12">
-                    <div class="card no-border d-flex flex-column justify-content-between border-radius-top shadow p-4 h-100">
-                        <p class="my-4 p-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                        <form class="mt-3" action="{{route('admin.restaurants.destroy', ['restaurant' => $restaurant->id])}}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger text-uppercase">
-                                Delete restaurant
-                            </button>
-                        </form>
+                    <div class="col-md-8 col-sm-12">
+                        <div class="card no-border d-flex flex-column justify-content-between border-radius-top shadow p-4 h-100">
+                            <p class="my-4 p-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            <form class="mt-3" action="{{route('admin.restaurants.destroy', ['restaurant' => $restaurant->id])}}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger text-uppercase">
+                                    Delete restaurant
+                                </button>
+                            </form>
+                        </div>
                     </div>
+
                 </div>
             </div>
 
