29a3568053c7cf933c9b949e8a1d07df2252be33
PatriziaCrimi <crimi.patrizia@gmail.com>
2021-02-17 10:40:47 +0100

    Migration creation Restaurants & Dishes updated with img_path

--- database/migrations/2021_02_12_153221_create_restaurants_table.php
+++ database/migrations/2021_02_12_153221_create_restaurants_table.php
@@ -6,29 +6,30 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateRestaurantsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('restaurants', function (Blueprint $table) {
-            $table->id();
-            $table->string('restaurant_name', 30);
-            $table->string('city', 20);
-            $table->string('address', 50);
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('restaurants', function (Blueprint $table) {
+      $table->id();
+      $table->string('restaurant_name', 30);
+      $table->string('city', 20);
+      $table->string('address', 50);
+      $table->string('img_path_rest');
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('restaurants');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('restaurants');
+  }
 }
--- database/seeds/DishesTableSeeder.php
+++ database/seeds/DishesTableSeeder.php
@@ -6,23 +6,24 @@ use App\Restaurant;
 
 class DishesTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        $dishes = config('dishes');
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    $dishes = config('dishes');
 
-        foreach ($dishes as $dish) {
-            $new_dish_obj = new Dish();
-            $new_dish_obj->name = $dish['name'];
-            $new_dish_obj->infos = $dish['infos'];
-            $new_dish_obj->visible = $dish['visible'];
-            $new_dish_obj->price = $dish['price'];
-            $new_dish_obj->restaurant_id = $dish['restaurant_id'];
-            $new_dish_obj->save();
-        }
+    foreach ($dishes as $dish) {
+      $new_dish_obj = new Dish();
+      $new_dish_obj->name = $dish['name'];
+      $new_dish_obj->infos = $dish['infos'];
+      $new_dish_obj->visible = $dish['visible'];
+      $new_dish_obj->price = $dish['price'];
+      $new_restaurant_obj->img_path_dish = $restaurant['img_path_dish'];
+      $new_dish_obj->restaurant_id = $dish['restaurant_id'];
+      $new_dish_obj->save();
     }
+  }
 }
