3db61bb82e08be71bedcf8c6c4eb7dc4e18791b1
Sabrina <sabrinacunsolo85@gmail.com>
2021-02-17 10:59:41 +0100

    inserite img path in file config + img in public

--- config/types.php
+++ config/types.php
@@ -21,4 +21,20 @@ return [
     "type" => "Organic food",
     "notes" => "",
   ],
+  [
+    "type" => "Street food",
+    "notes" => "",
+  ],
+  [
+    "type" => "Asian food",
+    "notes" => "",
+  ],
+  [
+    "type" => "Mexican",
+    "notes" => "",
+  ],
+  [
+    "type" => "Hawaian food",
+    "notes" => "",
+  ],
 ];
