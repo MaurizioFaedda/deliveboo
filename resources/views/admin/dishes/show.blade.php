6b2a72d6fe9247c467f0872f3f1e9fb6de7829ab
Maurizio <maurizio.faedda1@gmail.com>
2021-02-17 21:28:31 +0100

    Dish show

--- resources/views/admin/dishes/show.blade.php
+++ resources/views/admin/dishes/show.blade.php
@@ -2,30 +2,69 @@
 
 @section('content')
   <div class="container">
-    <div class="card">
-      <button type="button" name="button">
-        <a href="{{ route('admin.restaurants.show', ['restaurant' => $restaurant]) }}">
-          Torna indietro al ristorante
-        </a>
-      </button>
-      <p>
-        {{ $dish->name}}
-      </p>
-      <p>
-        {{ $dish->infos}}
-      </p>
-      <p>
-        {{ $dish->price}}
-      </p>
-      <p>
-        {{ $dish->visibile}}
-      </p>
+    <div class="card px-3 py-4">
+        <div class="d-flex justify-content-between">
+            <button class="back-link btn btn-link w-25 text-left pl-0" type="button" name="button">
+                <a href="{{ route('admin.restaurants.show', ['restaurant' => $restaurant]) }}">
+                    <span class="icon-back"></span>
+                </a>
+            </button>
+            <div class="text-right">
+                <a class="text-decoration-none" href="{{ route('admin.dishes.edit', ['dish'=> $dish->id]) }}">
+                    <span class="icon-edit"></span>
+                </a>
+                <form class="d-inline-block" action="{{ route('admin.dishes.destroy', ['dish' => $dish->id]) }}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn btn-link delete-restaurant align-baseline" type="submit">
+                        <span class="icon-delete "></span>
+                    </button>
+                </form>
+            </div>
 
-      @if($dish->img_path_dish)
-        <img src="{{ asset('storage/' . $dish->img_path_dish)}}" alt="{{ $dish->name}}">
-      @else
-        <img src="{{ asset('img/img_not_available.png')}}" alt="{{ $dish->name}} not available">
-      @endif
+        </div>
+      <div class="text-center">
+
+      </div>
+      <div class="row d-flex">
+          <div class="col-md-4">
+              <div class="d-flex flex-column justify-content-between h-75">
+                  <div class="">
+                      <small><strong>Dish</strong></small>
+                      <h4>{{ $dish->name}}</h4>
+                  </div>
+                  <div class="">
+                      <small><strong>Info</strong></small>
+                      <p>{{ $dish->infos}}</p>
+                  </div>
+                  <div class="">
+                      <small><strong>Price</strong></small>
+                      <p>€ <strong>{{ $dish->price}}</strong></p>
+                  </div>
+                  <div class="">
+                      <small><strong>Status</strong></small>
+                      <p>
+                          Your dish is currently <strong>{{$dish->visibile ? "" : "not"}} visible</strong> in the menu.
+                      </p>
+                  </div>
+              </div>
+              <div class="">
+
+              </div>
+          </div>
+          <div class="col-md-8 text-right">
+              <div class="img-dish-show w-100">
+                  @if($dish->img_path_dish)
+                      <img class="w-100" src="{{ asset('storage/' . $dish->img_path_dish)}}" alt="{{ $dish->name}}">
+                  @else
+                      <img src="{{ asset('img/img_not_available.png')}}" alt="{{ $dish->name}} not available">
+                  @endif
+              </div>
+          </div>
+      </div>
+
+
+
     </div>
   </div>
 @endsection
