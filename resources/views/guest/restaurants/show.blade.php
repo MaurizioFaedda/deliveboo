65cff9fb21372bf8742ba99f2808d6e41c122e91
Maurizio <maurizio.faedda1@gmail.com>
2021-02-19 13:58:47 +0100

    Modal Card for each dish

--- resources/views/guest/restaurants/show.blade.php
+++ resources/views/guest/restaurants/show.blade.php
@@ -4,38 +4,106 @@
 @section("page-title", "Deliveboo | Restaurant")
 
 @section('content')
-<div class="container-fluid">
-  <h1>{{$restaurant->restaurant_name}}</h1>
+{{-- Restaurant infos --}}
+<div class="container">
+    <div class="row d-flex pb-5">
+        <div class="col-md-8 d-flex flex-column justify-content-center">
+            <h3>{{$restaurant->restaurant_name}}</h3>
+            <ul class="list-group">
+                <li class="list-group-item">{{$restaurant->address}}, {{$restaurant->city}}</li>
+                <li class="list-group-item">
+                    @forelse ($restaurant->types as $type)
+                        <span>{{ $type->type }}</span>{{ !$loop->last ? ',' : '' }}
+                    @empty
+                        <span>Non sono state inserite tipologie</span>
+                    @endforelse
+                </li>
+                <li class="list-group-item h-100">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                </li>
+
+            </ul>
+
+        </div>
+        <div class="col-md-4">
+            <div class="restaurant-details">
+                <div class="img-restaurant w-100">
+                    <img class="w-100 rounded" src="{{asset('storage/' . $restaurant->img_path_rest)}}" alt="{{$restaurant->restaurant_name}} picture">
+                </div>
+            </div>
+
+        </div>
+    </div>
   {{-- Restaurant details --}}
-  <div class="restaurant-details">
-    <img src="{{asset('storage/' . $restaurant->img_path_rest)}}" alt="{{$restaurant->restaurant_name}} picture">
-    <ul>
-      <li>{{$restaurant->city}}</li>
-      <li>{{$restaurant->address}}</li>
-      @forelse ($restaurant->types as $type)
-          <li>{{ $type->type }}</li>
-      @empty
-          <li>Non sono state inserite tipologie</li>
-      @endforelse
-    </ul>
-  </div>
   {{-- MENU --}}
-  <div class="restaurant-menu">
-    <h2>Menu</h2>
-    <ul>
-      @forelse ($restaurant->dishes as $dish)
-        {{-- Se il piatto è disponibile lo visualizzo --}}
-        @if ($dish->visible == 1)
-          <li>
-            <img src="{{asset('storage/' . $dish->img_path_dish)}}" alt="{{$dish->name}} picture">
-          </li>
-          <li>{{$dish->name}}</li>
-          <li>{{number_Format($dish->price, 2, ',', '')}} €</li>
-        @endif
-      @empty
-          <li>No dishes available</li>
-      @endforelse
-    </ul>
   </div>
+{{-- Menu --}}
+<div class="bg-my-light">
+    <div class="restaurant-menu">
+    <div class="container">
+            <div class="row">
+                {{-- dish table --}}
+                <div class="col-md-8 py-5">
+                    <h3>Menu</h3>
+                    <div class="cards-section my-4 d-flex justify-content-between flex-wrap">
+                        @forelse ($restaurant->dishes as $dish)
+                            {{-- Se il piatto è disponibile lo visualizzo --}}
+                            @if ($dish->visible == 1)
+
+                                    <div class="card-dish bg-white mt-3 d-flex justify-content-between align-items center">
+
+                                                <button type="button" class="btn btn-link d-flex" data-toggle="modal" data-target="#{{ str_replace([' ', '&', ','], '', $dish->name) }}">
+                                                    <div class="w-50 p-2 d-flex flex-column justify-content-center align-items-center">
+                                                        <h5 data-target="#title">{{$dish->name}}</h5>
+                                                        <span class="text-bold">{{number_Format($dish->price, 2, ',', '')}} €</span>
+                                                    </div>
+                                                    <div class="img-dish">
+                                                        <img class="rounded" src="{{asset('storage/' . $dish->img_path_dish)}}" alt="{{$dish->name}} picture">
+                                                    </div>
+                                                </button>
+                                    </div>
+
+                                    <!-- Modal -->
+                                    <div class="modal fade" id="{{ str_replace([' ', '&', ','], '', $dish->name) }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                                      <div class="modal-dialog modal-dialog-centered" role="document">
+                                            <div class="modal-content">
+                                              <div class="modal-header">
+                                                    <h5 class="modal-title">{{$dish->name}}</h5>
+
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                      <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <div class="img-dish-model w-100">
+                                                        <img class="rounded w-100 h-100" src="{{asset('storage/' . $dish->img_path_dish)}}" alt="{{$dish->name}} picture">
+                                                    </div>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                    <button type="button" class="btn btn-primary">Add dish</button>
+                                              </div>
+                                            </div>
+                                      </div>
+                                    </div>
+                            @endif
+                        @empty
+                            <li>No dishes available</li>
+                        @endforelse
+                    </div>
+
+                </div>
+
+
+                {{-- cart --}}
+                <div class="col-md-4">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="">
+
 </div>
 @endsection
