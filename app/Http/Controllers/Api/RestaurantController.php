9c5b59932950ffa93f8f420f2851644878a424be
Marco <markoka1@outlook.it>
2021-02-22 18:47:22 +0100

    Bug fixed restaurant's type duplicate

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -38,24 +38,31 @@ class RestaurantController extends Controller
   public function get_filtered_restaurants(Request $request)
   {
     $filtered_restaurants = [];
-    $restaurants_types_array = [];
+    $restaurants_types_obj = [];
+    $final_restaurants_array = [];
     // Prendo il parametro che ho passato tramite POST contenente l'array dei tipi selezionati dall'utente nella checkbox
     $checked_types = $request->checked;
     // Ciclo l'array di tipi selezionati dall'utente
     for ($i=0; $i < count($checked_types); $i++) {
       // QUERY per cercare il tipo corrente ciclato all'interno della tabella Types nel DB
       $type = Type::find($checked_types[$i]);
-      // Raccolgo in un array tutti i ristoranti con il tipo corrente ciclato
-      $restaurants_types_array = $type->restaurants; // relazione MANY TO MANY
-      // Ciclo l'array di ristoranti appena creato
-      foreach ($restaurants_types_array as $current_restaurant) {
+      // Raccolgo in un oggetto tutti i ristoranti con il tipo corrente ciclato
+      $restaurants_types_obj = $type->restaurants; // relazione MANY TO MANY
+      // Ciclo l'oggetto contenente i ristoranti che hanno il tipo ciclato
+      foreach ($restaurants_types_obj as $current_restaurant) {
+        // Aggiungo ogni oggetto/ristorante all'array finale dei ristoranti filtrati
         array_push($filtered_restaurants, $current_restaurant);
       }
     };
+    // Rimuovo i duplicati dall'array dei ristoranti filtrati
+    $temp_arr_remove_dupes = array_unique(array_column($filtered_restaurants, 'id'));
+    $final_restaurants_array = array_intersect_key($filtered_restaurants, $temp_arr_remove_dupes);
     // Creo l'oggetto JSON con l'array dei ristoranti che hanno i tipi selezionati dall'utente
     return response()->json([
       'success' => true,
-      'results' => $filtered_restaurants
+      'results' => $final_restaurants_array
     ]);
   }
+
+
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -5,122 +5,20 @@ var app = new Vue ({
       types: [],
       restaurants: [],
       dishes: [],
-      // selected_type: '',
-      // checked_types: [],
-      // filtered_restaurants: [],
-      // id_restaurant: [],
-      search_reset: '',
-      pizza_checked: '',
-      array_pizza_checked: [],
-      italian_checked: '',
-      array_italian_checked: [],
-      sushi_checked: '',
-      array_sushi_checked: [],
-      vegan_checked: '',
-      array_vegan_checked: [],
-      organic_checked: '',
-      array_organic_checked: [],
-      street_checked: '',
-      array_street_checked: [],
-      asian_checked: '',
-      array_asian_checked: [],
-      mexican_checked: '',
-      array_mexican_checked: [],
-      hawaian_checked: '',
-      array_hawaian_checked: [],
-      array_carrello: []
+      selected_type: '',
+      checked_types: [],
+      filtered_restaurants: [],
     },
     methods: {
     getAllRestaurants() {
-        // this.selected_type = '';
-        // this.restaurants = [];
-        // this.checked_types = [];
+        this.selected_type = '';
+        this.restaurants = [];
+        this.checked_types = [];
         // -------------------- AXIOS call for ALL Restaurants --------------------
         axios
         .get('/api/restaurants')
         .then(response => {
           this.restaurants = response.data.results;
-        });
-    },
-    getPizza() {
-        // -------------------- AXIOS call for FILTERED Restaurants by Type --------------------
-        axios
-        .get('/api/restaurants/' + '1')
-        .then(response => {
-          this.array_pizza_checked = response.data.results;
-          console.log(this.array_pizza_checked);
-        });
-    },
-    getItalianFood() {
-        // -------------------- AXIOS call for FILTERED Restaurants by Type --------------------
-        axios
-        .get('/api/restaurants/' + '2')
-        .then(response => {
-          this.array_italian_checked = response.data.results;
-          console.log(this.array_italian_checked); 
-        });
-    },
-    getSushi() {
-        // -------------------- AXIOS call for FILTERED Restaurants by Type --------------------
-        axios
-        .get('/api/restaurants/' + '3')
-        .then(response => {
-          this.array_sushi_checked = response.data.results;
-          console.log(this.array_sushi_checked);
-        });
-    },
-    getVeganFoods() {
-        // -------------------- AXIOS call for FILTERED Restaurants by Type --------------------
-        axios
-        .get('/api/restaurants/' + '4')
-        .then(response => {
-          this.array_vegan_checked = response.data.results;
-          console.log(this.array_vegan_checked);
-        });
-    },
-    getOrganicFoods() {
-        // -------------------- AXIOS call for FILTERED Restaurants by Type --------------------
-        axios
-        .get('/api/restaurants/' + '5')
-        .then(response => {
-          this.array_organic_checked = response.data.results;
-          console.log(this.array_organic_checked);
-        });
-    },
-    getStreetFoods() {
-        // -------------------- AXIOS call for FILTERED Restaurants by Type --------------------
-        axios
-        .get('/api/restaurants/' + '6')
-        .then(response => {
-          this.array_street_checked = response.data.results;
-          console.log(this.array_street_checked);
-        });
-    },
-    getAsianFoods() {
-        // -------------------- AXIOS call for FILTERED Restaurants by Type --------------------
-        axios
-        .get('/api/restaurants/' + '7')
-        .then(response => {
-          this.array_asian_checked = response.data.results;
-          console.log(this.array_asian_checked);
-        });
-    },
-    getMexican() {
-        // -------------------- AXIOS call for FILTERED Restaurants by Type --------------------
-        axios
-        .get('/api/restaurants/' + '8')
-        .then(response => {
-          this.array_mexican_checked = response.data.results;
-          console.log(this.array_mexican_checked);
-        });
-    },
-    getHawaianFoods() {
-        // -------------------- AXIOS call for FILTERED Restaurants by Type --------------------
-        axios
-        .get('/api/restaurants/' + '9')
-        .then(response => {
-          this.array_hawaian_checked = response.data.results;
-          console.log(this.array_hawaian_checked);
         });
     },
     getAllTypes() {
@@ -129,7 +27,6 @@ var app = new Vue ({
         .get('/api/types')
         .then(response => {
           this.types = response.data.results;
-          console.log(this.types);
         });
     },
     getAllDishes() {
@@ -140,77 +37,220 @@ var app = new Vue ({
           this.dishes = response.data.results;
         });
     },
-    search_reset(){
-        this.pizza_checked = 0;
-        this.italian_checked = 0;
-        this.sushi_checked = 0;
-        this.vegan_checked = 0;
-        this.organic_checked = 0;
-        this.street_checked = 0;
-        this.asian_checked = 0;
-        this.mexican_checked = 0;
-        this.hawaian_checked = 0;
-    }
-      // getFilteredRestaurants() {
-      //   this.restaurants = [];
-      //   // -------------------- AXIOS call for FILTERED Restaurants by Type --------------------
-      //   axios
-      //   .get('/api/restaurants/' + this.selected_type)
-      //   .then(response => {
-      //     this.restaurants = response.data.results;
-      //   });
-      // },
-      // getFilteredRestaurantsByTypes() {
-      //   var request = {
-      //     checked: this.checked_types
-      //   }
-      //   // Se l'array di checkbox è vuoto visualizzo in automatico tutti i ristoranti
-      //   if(this.checked_types.length == 0) {
-      //     this.getAllRestaurants()
-      //   } else {
-      //     // -------------------- AXIOS call for FILTERED Restaurants by Types --------------------
-      //     this.restaurants = [];
-      //     axios
-      //     .post('/api/restaurants/', request)
-      //     .then(response => {
-      //       // prendo i risultati della chiamata ajax e li salvo in un array di appoggio
-      //       this.restaurants = response.data.results;
-      //       // console.log(this.current_restaurants);
-      //       // Ciclo il risultato della chiamata
-      //       this.restaurants.forEach((currentrestaurants) => {
-      //         // per ogni sinsolo oggetto controllo che id del ristorante non sia già presente nell'array id_restaurant
-      //         if(!this.id_restaurant.includes(currentrestaurants.id)){
-      //
-      //           // se non è presente lo pusho nell'array id_restaurant
-      //           this.id_restaurant.push(currentrestaurants.id);
-      //           // console.log(this.id_restaurant);
-      //
-      //           // Dopo aver selezionato gli elementi che hanno passato il check dell'id del ristorante li pusho nell'array dei ristoranti filtrati
-      //           this.filtered_restaurants.push(currentrestaurants);
-      //           // console.log(this.filtered_restaurants);
-      //
-      //           // Attribuisco il risultato dei ristoranti filtrati all'array restaurants
-      //           this.restaurants = this.filtered_restaurants;
-      //           // console.log(this.restaurant);
-      //         }
-      //       });
-      //     });
-      //   }
-      // }
-
+      getFilteredRestaurants() {
+        this.restaurants = [];
+        // -------------------- AXIOS call for FILTERED Restaurants by Type --------------------
+        axios
+        .get('/api/restaurants/' + this.selected_type)
+        .then(response => {
+          this.restaurants = response.data.results;
+        });
+      },
+      getFilteredRestaurantsByTypes() {
+        var request = {
+          checked: this.checked_types
+        }
+        // Se l'array di checkbox è vuoto visualizzo in automatico tutti i ristoranti
+        if(this.checked_types.length == 0) {
+          this.getAllRestaurants()
+        } else {
+          // -------------------- AXIOS call for FILTERED Restaurants by Types --------------------
+          this.restaurants = [];
+          axios
+          .post('/api/restaurants/', request)
+          .then(response => {
+            // prendo i risultati della chiamata ajax e li salvo in un array di appoggio
+            this.restaurants = response.data.results;
+            console.log(this.restaurants);
+          });
+        }
+      }
     },
     mounted() {
       this.getAllRestaurants();
       this.getAllTypes();
-      this.getPizza();
-      this.getSushi();
-      this.getItalianFood();
-      this.getVeganFoods();
-      this.getOrganicFoods();
-      this.getStreetFoods();
-      this.getAsianFoods();
-      this.getMexican();
-      this.getHawaianFoods();
       this.getAllDishes();
     }
 });
+
+
+
+// var app = new Vue ({
+//     el: '#app',
+//     data: {
+//       types: [],
+//       restaurants: [],
+//       dishes: [],
+//       selected_type: '',
+//       // checked_types: [],
+//       // filtered_restaurants: [],
+//       // id_restaurant: [],
+//       // search_reset: '',
+//       // pizza_checked: '',
+//       // array_pizza_checked: [],
+//       // italian_checked: '',
+//       // array_italian_checked: [],
+//       // sushi_checked: '',
+//       // array_sushi_checked: [],
+//       // vegan_checked: '',
+//       // array_vegan_checked: [],
+//       // organic_checked: '',
+//       // array_organic_checked: [],
+//       // street_checked: '',
+//       // array_street_checked: [],
+//       // asian_checked: '',
+//       // array_asian_checked: [],
+//       // mexican_checked: '',
+//       // array_mexican_checked: [],
+//       // hawaian_checked: '',
+//       // array_hawaian_checked: [],
+//       // array_carrello: []
+//     },
+//     methods: {
+//     getAllRestaurants() {
+//         // this.selected_type = '';
+//         // this.restaurants = [];
+//         // this.checked_types = [];
+//         // -------------------- AXIOS call for ALL Restaurants --------------------
+//         axios
+//         .get('/api/restaurants')
+//         .then(response => {
+//           this.restaurants = response.data.results;
+//         });
+//     },
+//     // getPizza() {
+//     //     // -------------------- AXIOS call for FILTERED Restaurants by Type --------------------
+//     //     axios
+//     //     .get('/api/restaurants/' + '1')
+//     //     .then(response => {
+//     //       this.array_pizza_checked = response.data.results;
+//     //       console.log(this.array_pizza_checked);
+//     //     });
+//     // },
+//     // getItalianFood() {
+//     //     // -------------------- AXIOS call for FILTERED Restaurants by Type --------------------
+//     //     axios
+//     //     .get('/api/restaurants/' + '2')
+//     //     .then(response => {
+//     //       this.array_italian_checked = response.data.results;
+//     //       console.log(this.array_italian_checked);
+//     //     });
+//     // },
+//     // getSushi() {
+//     //     // -------------------- AXIOS call for FILTERED Restaurants by Type --------------------
+//     //     axios
+//     //     .get('/api/restaurants/' + '3')
+//     //     .then(response => {
+//     //       this.array_sushi_checked = response.data.results;
+//     //       console.log(this.array_sushi_checked);
+//     //     });
+//     // },
+//     // getVeganFoods() {
+//     //     // -------------------- AXIOS call for FILTERED Restaurants by Type --------------------
+//     //     axios
+//     //     .get('/api/restaurants/' + '4')
+//     //     .then(response => {
+//     //       this.array_vegan_checked = response.data.results;
+//     //       console.log(this.array_vegan_checked);
+//     //     });
+//     // },
+//     // getOrganicFoods() {
+//     //     // -------------------- AXIOS call for FILTERED Restaurants by Type --------------------
+//     //     axios
+//     //     .get('/api/restaurants/' + '5')
+//     //     .then(response => {
+//     //       this.array_organic_checked = response.data.results;
+//     //       console.log(this.array_organic_checked);
+//     //     });
+//     // },
+//     // getStreetFoods() {
+//     //     // -------------------- AXIOS call for FILTERED Restaurants by Type --------------------
+//     //     axios
+//     //     .get('/api/restaurants/' + '6')
+//     //     .then(response => {
+//     //       this.array_street_checked = response.data.results;
+//     //       console.log(this.array_street_checked);
+//     //     });
+//     // },
+//     // getAsianFoods() {
+//     //     // -------------------- AXIOS call for FILTERED Restaurants by Type --------------------
+//     //     axios
+//     //     .get('/api/restaurants/' + '7')
+//     //     .then(response => {
+//     //       this.array_asian_checked = response.data.results;
+//     //       console.log(this.array_asian_checked);
+//     //     });
+//     // },
+//     // getMexican() {
+//     //     // -------------------- AXIOS call for FILTERED Restaurants by Type --------------------
+//     //     axios
+//     //     .get('/api/restaurants/' + '8')
+//     //     .then(response => {
+//     //       this.array_mexican_checked = response.data.results;
+//     //       console.log(this.array_mexican_checked);
+//     //     });
+//     // },
+//     // getHawaianFoods() {
+//     //     // -------------------- AXIOS call for FILTERED Restaurants by Type --------------------
+//     //     axios
+//     //     .get('/api/restaurants/' + '9')
+//     //     .then(response => {
+//     //       this.array_hawaian_checked = response.data.results;
+//     //       console.log(this.array_hawaian_checked);
+//     //     });
+//     // },
+//     // getAllTypes() {
+//     //     // -------------------- AXIOS call for all Types --------------------
+//     //     axios
+//     //     .get('/api/types')
+//     //     .then(response => {
+//     //       this.types = response.data.results;
+//     //       console.log(this.types);
+//     //     });
+//     // },
+//     // getAllDishes() {
+//     //     // -------------------- AXIOS call for all Dishes --------------------
+//     //     axios
+//     //     .get('/api/dishes')
+//     //     .then(response => {
+//     //       this.dishes = response.data.results;
+//     //     });
+//     // },
+//     // search_reset(){
+//     //     this.pizza_checked = 0;
+//     //     this.italian_checked = 0;
+//     //     this.sushi_checked = 0;
+//     //     this.vegan_checked = 0;
+//     //     this.organic_checked = 0;
+//     //     this.street_checked = 0;
+//     //     this.asian_checked = 0;
+//     //     this.mexican_checked = 0;
+//     //     this.hawaian_checked = 0;
+//     // }
+//       getFilteredRestaurants() {
+//         this.restaurants = [];
+//         // -------------------- AXIOS call for FILTERED Restaurants by Type --------------------
+//         axios
+//         .get('/api/restaurants/' + this.selected_type)
+//         .then(response => {
+//           this.restaurants = response.data.results;
+//         });
+//       },
+//
+//
+//     },
+//     mounted() {
+//       this.getAllRestaurants();
+//       this.getAllTypes();
+//       // this.getPizza();
+//       // this.getSushi();
+//       // this.getItalianFood();
+//       // this.getVeganFoods();
+//       // this.getOrganicFoods();
+//       // this.getStreetFoods();
+//       // this.getAsianFoods();
+//       // this.getMexican();
+//       // this.getHawaianFoods();
+//       this.getAllDishes();
+//     }
+// });
