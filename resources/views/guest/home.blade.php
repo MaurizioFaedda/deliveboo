4585c201858dd0260a1fefbb23b4d279d5abced3
Maurizio <maurizio.faedda1@gmail.com>
2021-02-23 11:50:25 +0100

    Card restaurant

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -34,18 +34,18 @@
         <div class="card custom_padding no-border my-4 shadow w-100 thiscartbody">
             <h1 class="search_title">Restaurants</h1>
             <div  class="row">
-                 <div v-for="restaurant in restaurants" class="col-sm col-md-4">
-                    <div class="card my-4 w-100 h-25">
+                 <div v-for="restaurant in restaurants" class="col-sm col-md-4 card-restaurant my-3">
+                    <div class="card my-4 w-100 no-border">
                         <a :href="'restaurant/' + restaurant.id">
-                            <img v-if="restaurant.img_path_rest" class="card-img-top g-custom-card-height" :src="'storage/' + restaurant.img_path_rest" alt="Card image cap">
+                            <img v-if="restaurant.img_path_rest" class="card-img-top g-custom-card-height w-100" :src="'storage/' + restaurant.img_path_rest" alt="Card image cap">
                             <img v-else class="card-img-top g-custom-card-height" src="" alt="Card image cap">
                         </a>
-                        <div class="card-body">
-                            <h5 class="card-title font-weight-bold"> @{{restaurant.restaurant_name}}</h5>
-                            <p class="card-text">
+                        <div class="card-body pl-0">
+                            <h5 class="card-title font-weight-bold"><span class="ml-1 icon-restaurant-main-color"></span> @{{restaurant.restaurant_name}}</h5>
+                            <p class="card-text p-infos">
                                 @{{restaurant.description}}
                             </p>
-                            <a :href="'restaurant/' + restaurant.id" class="btn btn-primary">Visualizza ristorante</a>
+                            <a :href="'restaurant/' + restaurant.id" class="btn btn-primary">Show more</a>
                         </div>
                     </div>
                 </div>
