20f7e616b5b3874e1e50eadcf3c2dff30052a8e3
Sabrina <sabrinacunsolo85@gmail.com>
2021-02-25 10:58:01 +0100

    create PaymentsTableSeeder

---
+++ database/seeds/PaymentsTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Order;
+use App\Payment;
+
+class PaymentsTableSeeder extends Seeder
+{
+    /**
+    * Run the database seeds.
+    *
+    * @return void
+    */
+    public function run()
+    {
+        $payments = config('payment');
+
+        foreach ($payments as $payment) {
+            $new_payment_obj = new Payment();
+            $new_payment_obj->order_id = $payment['order_id'];
+            $new_payment_obj->card_owner = $payment['card_owner'];
+            $new_payment_obj->status = $payment['status'];
+            $new_payment_obj->method = $payment['method'];
+            $new_payment_obj->card_number = $payment['card_number'];
+            $new_payment_obj->notes = $payment['notes'];
+            $new_payment_obj->save();
+        }
+    }
+}
