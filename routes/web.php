75b45a434bced4a4855ade2fb5cf30b086eb84e2
Maurizio <maurizio.faedda1@gmail.com>
2021-02-25 21:12:05 +0100

    installer braintree

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,6 +22,57 @@ Route::get('/restaurant/{id}', 'RestaurantController@show')->name('restaurant.sh
 Route::resource('guest/orders', 'OrderController');
 // Payments routes
 Route::resource('/payments', 'PaymentController');
+
+Route::get('/pay', function () {
+    $gateway = new Braintree\Gateway([
+  'environment' => 'sandbox',
+  'merchantId' => 'gncpw48dy8pnxspp',
+  'publicKey' => '5sp8xkhmyk77xmpn',
+  'privateKey' => '66a03d2d8813d5bc80879e71650b8045'
+]);
+
+
+    $token = $gateway->ClientToken()->generate();
+
+    return view('guest.pay', [
+        'token' => $token
+    ]);
+});
+
+Route::post('/checkout', function(Request $request){
+    $gateway = new Braintree\Gateway([
+      'environment' => 'sandbox',
+      'merchantId' => 'gncpw48dy8pnxspp',
+      'publicKey' => '5sp8xkhmyk77xmpn',
+      'privateKey' => '66a03d2d8813d5bc80879e71650b8045'
+    ]);
+    $amount = $request->amount;
+    $nonce = $request->payment_method_nonce;
+    $result = $gateway->transaction()->sale([
+        'amount' => $amount,
+        'paymentMethodNonce' => $nonce,
+        'options' => [
+            'submitForSettlement' => true
+        ]
+    ]);
+
+    if ($result->success) {
+        $transaction = $result->transaction;
+        // header("Location: transaction.php?id=" . $transaction->id);
+
+        return back()->with('success_message', 'Transaction successful. The ID is:'. $transaction->id);
+    } else {
+        $errorString = "";
+
+        foreach ($result->errors->deepAll() as $error) {
+            $errorString .= 'Error: ' . $error->code . ": " . $error->message . "\n";
+        }
+
+        // $_SESSION["errors"] = $errorString;
+        // header("Location: index.php");
+        return back()->withErrors('An error occurred with the message: '.$result->message);
+    }
+});
 
 // Cart Controller routes
 Route::get('/cart', 'CartController@index')->name('cart.index');
