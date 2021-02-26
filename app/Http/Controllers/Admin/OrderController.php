0f392704f60c2217edf54874ce36f1931a590e6e
Annalisa <annalisadesantis770@gmail.com>
2021-02-26 20:14:33 +0100

    Sviluppo dei dati ai fini statistici in OrderController

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Restaurant;
 use App\User;
 use App\Order;
+use App\Payment;
 
 class OrderController extends Controller
 {
@@ -24,17 +25,51 @@ class OrderController extends Controller
         // Faccio una query al db per prendere tutti i ristoranti con l'id user dell'utente loggato
         $restaurants_list = Restaurant::select()->where('user_id', $id_user)->get();
 
-        // Faccio una query al db per prendere tutti gli ordini dei ristoranti dell'utente loggato
-        $order_list = Order::select()->whereIn('restaurant_id' , Restaurant::select('id')->where('user_id', $id_user))->get();
+        // Faccio una query al db per prendere tutti gli ordini dei ristoranti dell'utente loggato dove i pagamenti sono andati a buon fine
+        $order_list = Order::select()->whereIn('restaurant_id' , Restaurant::select('id')->where('user_id', $id_user))->whereIn('id' , Payment::select('order_id')->where('status', 'Accepted'))->get();
 
-        $prova = [7,3,8,5,6];
+
+        // Faccio una query al db per prendere tutte le date di delivery time degli ordini dei ristoranti dell'utente loggato dove i pagamenti sono andati a buon fine
+        $delivery_time = Order::select('delivery_time')->whereIn('restaurant_id' , Restaurant::select('id')->where('user_id', $id_user))->whereIn('id' , Payment::select('order_id')->where('status', 'Accepted'))->get();
+
+
+        // Creo un array di appoggio per salvarmi tutti i delivery time
+        $array_date = [];
+
+        // Ciclo la collection di delivery time per estrapolarmi tutte le date e pusharle in array_date
+        foreach ($delivery_time as $date) {
+            array_push($array_date, $date->delivery_time);
+        }
+
+        // Creo un array di appoggio per salvarmi tutti i mesi
+        $array_mounth = [];
+
+        // Ciclo l'array delle date per estrapolarmi i mesi e pusharli dentro l'$array_mounth
+        for ($i=0; $i < count($array_date); $i++) {
+            $mounth = date("m", strtotime($array_date[$i]));
+            array_push($array_mounth, $mounth);
+        }
+
+        // Ordino l'array dei mesi in modo crescente
+        asort($array_mounth);
+
+        // Faccio un count di quante volte c'è una data significa che è la quantità di ordini
+        $count = array_count_values($array_mounth);
+
+        // Creo un array dove ci sono i numeri dei mesi dell'anno 
+        $mounth = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
+
+        // Faccio un match tra i mesi dell'anno e i mesi presenti nell'array dei mesi degli ordini
+        $match_results = array_intersect ($mounth, $array_mounth);
+
+
 
         $data = [
             // Lista ristoranti dell'utente
             'restaurants' => $restaurants_list,
-            // Lista degli ordini di tutti i ristoranti dell'utente
+            // Lista degli ordini con pagamento ok di tutti i ristoranti dell'utente
             'orders' => $order_list,
-            'prova' => $prova
+            'count' => $count
         ];
 
         return view('admin.orders.index', $data);
