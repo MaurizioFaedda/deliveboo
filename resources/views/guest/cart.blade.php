81463f7a921850d26f8e2fc3e750805e0ec94487
Maurizio <maurizio.faedda1@gmail.com>
2021-02-26 13:14:48 +0100

    Form order/payment

--- resources/views/guest/cart.blade.php
+++ resources/views/guest/cart.blade.php
@@ -17,136 +17,123 @@
             @endif
             <div class="row d-flex">
                 <div class="col-md-7 text-dark ">
-                  {{-- FORM per le informazioni sulla consegna | Backend ORDERS table --}}
-                  <form class="bg-white px-4 py-3" action="{{route('orders.store')}}" method="post">
-                    @csrf
-                      <div class="form-row pb-4">
-                          <div class="form-group col-md-6">
-                            <label for="first_name">First Name</label>
-                            <input type="text" class="form-control" placeholder="First Name" name="first_name" value="{{old('first_name')}}" required>
-                            {{-- SHOWING ERROR MESSAGE --}}
-                            @error('first_name')
-                                <div class="alert alert-danger">
-                                    {{ $message }}
+                  {{-- <form class="bg-white px-4 py-3" action="{{route('orders.store')}}" method="post"> --}}
+                    <form class="bg-white p-3" method="post" id="payment-form" action="{{ url('/checkout')}}">
+                        @csrf
+                        <div class="form-row pb-4">
+                               {{-- SEZIONE per le informazioni degli ORDINI | Backend ORDERS table --}}
+                                <div class="form-group col-md-6">
+                                    <label for="first_name">First Name</label>
+                                    <input type="text" class="form-control" placeholder="First Name" name="first_name" value="{{old('first_name')}}" required>
+                                    {{-- SHOWING ERROR MESSAGE --}}
+                                    @error('first_name')
+                                        <div class="alert alert-danger">
+                                            {{ $message }}
+                                        </div>
+                                    @enderror
                                 </div>
-                            @enderror
-                          </div>
-                          <div class="form-group col-md-6">
-                            <label for="lastname">Last Name</label>
-                            <input type="text" class="form-control" placeholder="Last Name" name="lastname" value="{{old('lastname')}}" required>
-                            {{-- SHOWING ERROR MESSAGE --}}
-                            @error('lastname')
-                                <div class="alert alert-danger">
-                                    {{ $message }}
+                                <div class="form-group col-md-6">
+                                    <label for="lastname">Last Name</label>
+                                    <input type="text" class="form-control" placeholder="Last Name" name="lastname" value="{{old('lastname')}}" required>
+                                    {{-- SHOWING ERROR MESSAGE --}}
+                                    @error('lastname')
+                                        <div class="alert alert-danger">
+                                            {{ $message }}
+                                        </div>
+                                    @enderror
                                 </div>
-                            @enderror
-                          </div>
-                          <div class="form-group col-md-6">
-                            <label for="mobile">Phone Number</label>
-                            <input type="text" class="form-control" placeholder="e.g. +39 320 000 0000" name="mobile" value="{{old('mobile')}}" required>
-                            {{-- SHOWING ERROR MESSAGE --}}
-                            @error('mobile')
-                                <div class="alert alert-danger">
-                                    {{ $message }}
+                                <div class="form-group col-md-6">
+                                    <label for="mobile">Phone Number</label>
+                                    <input type="text" class="form-control" placeholder="e.g. +39 320 000 0000" name="mobile" value="{{old('mobile')}}" required>
+                                    {{-- SHOWING ERROR MESSAGE --}}
+                                    @error('mobile')
+                                        <div class="alert alert-danger">
+                                            {{ $message }}
+                                        </div>
+                                    @enderror
                                 </div>
-                            @enderror
-                          </div>
-                          <div class="form-group col-md-6">
-                            <label for="email">Email</label>
-                            <input type="email" class="form-control" placeholder="example@mail.com" name="email" value="{{old('email')}}" required>
-                            {{-- SHOWING ERROR MESSAGE --}}
-                            @error('email')
-                                <div class="alert alert-danger">
-                                    {{ $message }}
+                                <div class="form-group col-md-6">
+                                    <label for="email">Email</label>
+                                    <input type="email" class="form-control" placeholder="example@mail.com" name="email" value="{{old('email')}}" required>
+                                    {{-- SHOWING ERROR MESSAGE --}}
+                                    @error('email')
+                                        <div class="alert alert-danger">
+                                            {{ $message }}
+                                        </div>
+                                    @enderror
                                 </div>
-                            @enderror
-                          </div>
-                          <div class="form-group col-md-6">
-                            <label>City</label>
-                            <input readonly type="text" class="form-control" placeholder="Rome">
-                          </div>
-                          <div class="form-group col-md-6">
-                            <label for="address">Address</label>
-                            <input type="text" class="form-control" placeholder="Via Italia, 155" name="address" value="{{old('address')}}" required>
-                            {{-- SHOWING ERROR MESSAGE --}}
-                            @error('address')
-                                <div class="alert alert-danger">
-                                    {{ $message }}
+                                <div class="form-group col-md-6">
+                                    <label>City</label>
+                                    <input readonly type="text" class="form-control" placeholder="Rome">
                                 </div>
-                            @enderror
-                          </div>
-                          <div class="form-group col-md-12">
-                            <label for="delivery_time">Delivery time</label>
-                            <input type="datetime-local" class="form-control" placeholder="The doorbell does not work" name="delivery_time" value="{{old('delivery_time')}}" required>
-                            {{-- SHOWING ERROR MESSAGE --}}
-                            @error('delivery_time')
-                                <div class="alert alert-danger">
-                                    {{ $message }}
+                                <div class="form-group col-md-6">
+                                    <label for="address">Address</label>
+                                    <input type="text" class="form-control" placeholder="Via Italia, 155" name="address" value="{{old('address')}}" required>
+                                    {{-- SHOWING ERROR MESSAGE --}}
+                                    @error('address')
+                                        <div class="alert alert-danger">
+                                            {{ $message }}
+                                        </div>
+                                    @enderror
                                 </div>
-                            @enderror
-                          </div>
-                          <div class="form-group col-md-12">
-                            <label for="notes">Information for the rider</label>
-                            <input type="text" class="form-control" placeholder="The doorbell does not work" name="notes" value="{{old('notes')}}">
-                            {{-- SHOWING ERROR MESSAGE --}}
-                            @error('notes')
-                                <div class="alert alert-danger">
-                                    {{ $message }}
+                                <div class="form-group col-md-12">
+                                    <label for="delivery_time">Delivery time</label>
+                                    <input type="datetime-local" class="form-control" placeholder="The doorbell does not work" name="delivery_time" value="{{old('delivery_time')}}" required>
+                                    {{-- SHOWING ERROR MESSAGE --}}
+                                    @error('delivery_time')
+                                        <div class="alert alert-danger">
+                                            {{ $message }}
+                                        </div>
+                                    @enderror
                                 </div>
-                            @enderror
-                          </div>
-                          <div class="form-group col-md-6">
-                            <label for="total_price">Total price</label>
-                            <input readonly class="form-control" :placeholder="totalPrice" name="total_price" :value="totalPrice">
-                          </div>
-                          {{-- Hidden inputs for backend only --}}
-                          <div class="form-group d-none">
-                            <label for="restaurant_id">Restaurant</label>
-                            <input type="hidden" class="form-control" name="restaurant_id" :value="cart_list[0].restaurant_id">
-                          </div>
-                          {{-- BUTTON to send data to backend --}}
-                          <div class="form-group d-flex justify-content-end">
-                              <button type="submit" class="btn btn-success text-uppercase form-font shadow">
-                                  Submit
-                              </button>
-                          </div>
-                      </div>
-                  </form>
-                  <hr>
-                  {{-- FORM per le informazioni sul pagamento | Backend PAYMENTS table --}}
-                  @if (session('success_message'))
-                          <div class="alert alert-success">
-                              {{ session('success_message') }}
-                          </div>
-                      @endif
+                                <div class="form-group col-md-12">
+                                    <label for="notes">Information for the rider</label>
+                                    <input type="text" class="form-control" placeholder="The doorbell does not work" name="notes" value="{{old('notes')}}">
+                                    {{-- SHOWING ERROR MESSAGE --}}
+                                    @error('notes')
+                                        <div class="alert alert-danger">
+                                            {{ $message }}
+                                        </div>
+                                    @enderror
+                                </div>
+                              {{-- Hidden inputs for backend only --}}
+                                <div class="form-group col-md-6">
+                                      <input type="hidden" class="form-control" name="total_price" :value="totalPrice">
+                                </div>
+                                <div class="form-group d-none">
+                                    <label for="restaurant_id">Restaurant</label>
+                                    <input type="hidden" class="form-control" name="restaurant_id" :value="cart_list[0].restaurant_id">
+                                </div>
+                        </div>
+                        <hr>
+                          {{-- SEZIONE per le informazioni sul pagamento | Backend PAYMENTS table --}}
+                            <div class="form-group col-md-6 p-0">
+                              <label for="card_owner"></label>
+                              <input type="text" class="form-control" placeholder="Full Name" name="card_owner" value="{{old('card_owner')}}" required>
+                              {{-- SHOWING ERROR MESSAGE --}}
+                              @error('card_owner')
+                                  <div class="alert alert-danger">
+                                      {{ $message }}
+                                  </div>
+                              @enderror
+                            </div>
+                          {{-- BRAINTREE SECTION --}}
+                        <section>
+                            <label for="amount">
+                             <span class="input-label">Amount</span>
+                             <div class="input-wrapper amount-wrapper">
+                                 <input id="amount" name="amount" type="tel" min="1" placeholder="Amount" readonly :value="totalPrice">
+                             </div>
+                            </label>
 
-                      @if(count($errors) > 0)
-                          <div class="alert alert-danger">
-                              <ul>
-                                  @foreach ($errors->all() as $error)
-                                      <li>{{ $error }}</li>
-                                  @endforeach
-                              </ul>
-                          </div>
-                      @endif
-                      <form class="bg-white p-3" method="post" id="payment-form" action="{{ url('/checkout')}}">
-                          @csrf
-                             <section>
-                                 <label for="amount">
-                                     <span class="input-label">Amount</span>
-                                     <div class="input-wrapper amount-wrapper">
-                                         <input id="amount" name="amount" type="tel" min="1" placeholder="Amount" readonly :value="totalPrice">
-                                     </div>
-                                 </label>
+                            <div class="bt-drop-in-wrapper">
+                                <div id="bt-dropin"></div>
+                            </div>
+                        </section>
 
-                                 <div class="bt-drop-in-wrapper">
-                                     <div id="bt-dropin"></div>
-                                 </div>
-                             </section>
-
-                             <input id="nonce" name="payment_method_nonce" type="hidden" />
-                             <button class="btn btn-primary" type="submit"><span>Pay</span></button>
-                      </form>
+                        <input id="nonce" name="payment_method_nonce" type="hidden" />
+                        <button class="btn btn-primary" type="submit"><span>Pay</span></button>
+                    </form>
                 </div>
                 <div class="col-5">
                     <table class="table bg-white">
@@ -192,7 +179,7 @@
           selector: '#bt-dropin',
           paypal: {
             flow: 'vault'
-          }
+        },
         }, function (createErr, instance) {
           if (createErr) {
             console.log('Create Error', createErr);
