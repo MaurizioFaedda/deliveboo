e4672a22917f5dd0e606cdd9cf3dee03cfdb8347
Marco <markoka1@outlook.it>
2021-03-01 10:51:56 +0100

    shadow fix

--- resources/views/admin/restaurants/index.blade.php
+++ resources/views/admin/restaurants/index.blade.php
@@ -7,8 +7,8 @@
 <div class="container">
     <div class="row">
         <div class="col-md-6 col-sm-12">
-            <div class="card w-100 shadow no-border">
-                <a href="{{route('admin.restaurants.create')}}" class="btn btn-primary font-weight-bold m-3 shadow mx-auto o-border w-75">
+            <div class="card w-100 my-shadow no-border">
+                <a href="{{route('admin.restaurants.create')}}" class="btn btn-primary font-weight-bold m-3 my-shadow mx-auto border-0 w-75">
                     Create a new restaurant
                 </a>
             </div>
@@ -19,14 +19,16 @@
     <div class="row justify-content-left">
         @foreach ($restaurants as $restaurant)
             <div class="col-md-6">
-                <div class="card my-4 shadow border-0">
+                <div class="card my-4 my-shadow border-0">
                     @if($restaurant->img_path_rest)
-                        <img class="card-img-top custom-height p-4" src="{{ asset('storage/' . $restaurant->img_path_rest)}}" alt="Card image cap" alt="{{$restaurant->restaurant_name}}">
+                        <a href="{{ route('admin.restaurants.show', ['restaurant' => $restaurant->id]) }}">
+                            <img class="card-img-top custom-height p-4" src="{{ asset('storage/' . $restaurant->img_path_rest)}}" alt="Card image cap" alt="{{$restaurant->restaurant_name}}">
+                        </a>
                     @else
                         <img class="card-img-top" src="{{ asset('img/img_not_available.png')}}" alt="not img">
                     @endif
                     <div class="card-body">
-                        <h5 class="card-title font-weight-bold text-uppercase">{{$restaurant->restaurant_name}}</h5>
+                        <h5 class="card-title font-weight-bold text-uppercase p-2">{{$restaurant->restaurant_name}}</h5>
                         <a href="{{ route('admin.restaurants.show', ['restaurant' => $restaurant->id]) }}" class="btn btn-primary w-100 font-weight-bold">Show restaurant</a>
                     </div>
                 </div>
