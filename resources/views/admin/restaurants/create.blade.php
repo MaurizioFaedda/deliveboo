c8d507106e22c8061797a8be6519bc8bd5377e47
Marco <markoka1@outlook.it>
2021-03-01 18:36:43 +0100

    fix visivi

--- resources/views/admin/restaurants/create.blade.php
+++ resources/views/admin/restaurants/create.blade.php
@@ -8,14 +8,14 @@
     <div class="row justify-content-center">
       <div class="col-md-8">
         <div class="card no-border mt-3 border-radius-top shadow">
-          <div class="card-header card-header text-center no-border border-radius-top form-font">Add a new resturant</div>
+          <div class="card-header card-header text-center no-border border-radius-top nunito">Add a new resturant</div>
           <form class="d-flex flex-column align-items-center py-5" action="{{route('admin.restaurants.store')}}" method="post" enctype="multipart/form-data">
             @csrf
             <div class="card border-0 mb-5 w-75">
                 <div class="card-body shadow p-3">
                     <div class="form-group d-flex flex-column w-100">
-                        <label class="form-font" for="title">Restaurant Name</label>
-                        <input class="form-check form-font" type="text" class="form-control" name="restaurant_name" placeholder="Write your restaurant name here" value="{{old('restaurant_name')}}" required maxlength="100">
+                        <label class="nunito" for="title">Restaurant Name</label>
+                        <input class="form-check nunito" type="text" class="form-control" name="restaurant_name" placeholder="Write your restaurant name here" value="{{old('restaurant_name')}}" required maxlength="100">
                         {{-- SHOWING ERROR MESSAGE --}}
                         @error('restaurant_name')
                             <div class="alert alert-danger">
@@ -24,8 +24,8 @@
                         @enderror
                     </div>
                     <div class="form-group d-flex flex-column w-100">
-                        <label class="form-font" for="title">City</label>
-                        <input class="form-check form-font" type="text" class="form-control" name="city" placeholder="City" value="{{old('city')}}" required maxlength="20">
+                        <label class="nunito" for="title">City</label>
+                        <input class="form-check nunito" type="text" class="form-control" name="city" placeholder="City" value="{{old('city')}}" required maxlength="20">
                         {{-- SHOWING ERROR MESSAGE --}}
                         @error('city')
                             <div class="alert alert-danger">
@@ -34,8 +34,8 @@
                         @enderror
                     </div>
                     <div class="form-group d-flex flex-column w-100">
-                        <label class="form-font" for="title">Address</label>
-                        <input class="form-check form-font" type="text" class="form-control" name="address" placeholder="Full address" value="{{old('address')}}" required maxlength="50">
+                        <label class="nunito" for="title">Address</label>
+                        <input class="form-check nunito" type="text" class="form-control" name="address" placeholder="Full address" value="{{old('address')}}" required maxlength="50">
                         {{-- SHOWING ERROR MESSAGE --}}
                         @error('address')
                             <div class="alert alert-danger">
@@ -44,7 +44,7 @@
                         @enderror
                     </div>
                     <div class="form-group d-flex flex-column w-100">
-                        <label class="form-font" for="title">Description</label>
+                        <label class="nunito" for="title">Description</label>
                         <textarea name="description" rows="8" cols="80">{{ old('description')}}</textarea>
                         {{-- SHOWING ERROR MESSAGE --}}
                         @error('description')
@@ -57,7 +57,7 @@
             </div>
             <div class="card border-0 mb-5 w-75">
                 <div class="card-body shadow p-3">
-                    <h2 class="h4 mb-2 py-2 form-font font-weight-bold">Choise a types of your restaurant</h2>
+                    <h2 class="h4 mb-2 py-2 nunito font-weight-bold">Select your restaurants types</h2>
                     @foreach ($types as $type)
                         <div class="form-check  form-font">
                             <input type="checkbox" name="types[]" value="{{$type->id}}"
@@ -78,7 +78,7 @@
             <div class="card border-0 mb-5 w-75">
                 <div class="card-body shadow p-3">
                     <div class="form-group d-flex flex-column align-items-left w-75">
-                        <h2 class="h4 mb-2 py-2 form-font font-weight-bold">Insert a picture</h2>
+                        <h2 class="h4 mb-2 py-2 form-font font-weight-bold">Upload a picture</h2>
                         <input class="pl-1 form-font" type="file" class="form-control-file" name="image" accept="image/*">
                     </div>
                 </div>
